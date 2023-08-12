27dcded6fa92363944434dd12f9ed948d719827b
fanndo <fernando.telep@gmail.com>
2023-08-12 22:52:29 +0000

    sintaxis

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.use('/categories', categoryRoute);
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}.`)
 })
